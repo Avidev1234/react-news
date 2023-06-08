ad5ba14d8cd9d3fc4ac4e3c374f2c9c5d14bc37a
Avidev Jha (SSB Digital) <110378319+Avidev1234@users.noreply.github.com>
2023-06-08 19:58:58 +0530

    Updates

---
+++ precache-manifest.dfa575c0e894a264ad9b7197baddd278.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cf214ee262d7a871b6d",
+    "url": "/react-news/static/css/main.c0c50ee4.chunk.css"
+  },
+  {
+    "revision": "8cf214ee262d7a871b6d",
+    "url": "/react-news/static/js/main.8cf214ee.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/react-news/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "e79a69ef1d7744949d13",
+    "url": "/react-news/static/js/2.e79a69ef.chunk.js"
+  },
+  {
+    "revision": "b3cf2ab094378ebf7584",
+    "url": "/react-news/static/js/runtime~main.b3cf2ab0.js"
+  },
+  {
+    "revision": "b832be75291ca184796c7da19769aaa5",
+    "url": "/react-news/static/media/loading.b832be75.gif"
+  },
+  {
+    "revision": "c6c6145ca43eb72a89946b528a28f12e",
+    "url": "/react-news/index.html"
+  }
+];
